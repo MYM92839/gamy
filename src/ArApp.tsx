5db548eea6363a67a81bb573598dcb33d3a2c1e5
mym <mym92839@gmail.com>
2025-01-19 00:49:24 +0900

    23

--- src/ArApp.tsx
+++ src/ArApp.tsx
@@ -115,7 +115,7 @@ const customStyles = {
 Modal.setAppElement('#root');
 
 const CircularMask = () => (
-  <group scale={[1, 1, 1]} position={[0.35, 0.48, -0.017]}>
+  <group scale={[1, 1, 1]} position={[0, 0.48, -0.017]}>
     <Mask id={1} colorWrite={false} depthWrite={false}>
       <planeGeometry args={[0.6, 1]} />
     </Mask>
@@ -164,7 +164,7 @@ function Tree({
     <group
       ref={modelRef}
       scale={[0.5, 0.5, 0.5]}
-      position={[0.35, 0, 0]}
+      position={[0, 0, 0]}
       rotation={[0, 0, 0]}
       {...props}
       dispose={null}
