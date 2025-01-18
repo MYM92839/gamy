715770df0cd05732633f5c027817ea36c4bd435f
mym <mym92839@gmail.com>
2025-01-18 16:25:03 +0900

    asf

--- src/ArApp.tsx
+++ src/ArApp.tsx
@@ -180,7 +180,7 @@ function Box({ onRenderEnd, ...props }: JSX.IntrinsicElements['group'] & { onRen
   }
 
   return (
-    <group ref={modelRef} {...props} scale={[0.1, 0.1, 0.1]} position={[0, 0, 0]} dispose={null}>
+    <group ref={modelRef} {...props} scale={[1, 1, 1]} position={[0, 0, 0]} dispose={null}>
       <group name="Scene">
         <group name="Group001">
           <group name="DeformationSystem001">
