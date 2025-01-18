18d008f4c42a0796abf837bbbaff8a6173308738
mym <mym92839@gmail.com>
2025-01-18 17:01:46 +0900

    asdf

--- src/ArApp.tsx
+++ src/ArApp.tsx
@@ -180,7 +180,7 @@ function Box({ onRenderEnd, ...props }: JSX.IntrinsicElements['group'] & { onRen
   }
 
   return (
-    <group ref={modelRef} {...props} scale={[0.5, 0.5, 0.5]} position={[14, -3, -6.5]} dispose={null}>
+    <group ref={modelRef} {...props} scale={[0.1, 0.1, 0.1]} position={[0, 0, 0]} dispose={null}>
       <group name="Scene">
         <group name="Group001">
           <group name="DeformationSystem001">
@@ -508,7 +508,7 @@ export default function ArApp() {
           width: '100%',
           height: '100%',
           position: 'absolute',
-          // zIndex: 100,
+          zIndex: 100,
         }}
         camera={{
           position: [0, 0, 10],
