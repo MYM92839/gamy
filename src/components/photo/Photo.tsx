27cac29141cfab8ce7327cc7c3f37afdefc9f8de
mym <mym92839@gmail.com>
2025-01-19 12:17:05 +0900

    adf

--- src/components/photo/Photo.tsx
+++ src/components/photo/Photo.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Photo() {
   return (<div className="w-full h-full  min-h-[874px] relative">
-    <img className="absolute bottom-0 w-full" src={import.meta.env.VITE_PUBLIC_URL + "/imgs/background_image.png"} />
+    <img className="absolute bottom-0 w-full" src={import.meta.env.VITE_PUBLIC_URL + "/background_image.png"} />
     <div className="absolute inset-0 bg-gradient-to-b from-[#344173] via-[#344173]/60 to-[#344173]/30 via-70%" />
     <Outlet />
   </div>)
--- src/components/photo/PhotoPara.tsx
+++ src/components/photo/PhotoPara.tsx
@@ -6,7 +6,7 @@ interface IPhotoParaProps extends PropsWithChildren {
 export default function PhotoPara({ title, children }: IPhotoParaProps) {
   return (
     <div className="absolute inset-0 px-[24px]">
-      <img className="mt-[60px] mx-auto" src={import.meta.env.VITE_PUBLIC_URL + "/imgs/title.png"} />
+      <img className="mt-[60px] mx-auto" src={import.meta.env.VITE_PUBLIC_URL + "/title.png"} />
       <div className="mt-[57px] font-semibold text-[#EBF0E8] text-[16px] mx-auto text-center whitespace-pre-wrap">{title}</div>
       <div className="mt-[125px] w-full text-center leading-[25px] text-[16px] font-medium py-[11px] rounded-[20px] border border-white bg-black/30 backdrop-blur-sm text-white">
         카메라 사용을 허용해야<br />
