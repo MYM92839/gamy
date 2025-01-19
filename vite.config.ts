067610774dd6046c1656e8a935cec8a937b58c2b
mym <mym92839@gmail.com>
2025-01-19 11:55:21 +0900

    asdf

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
-  base: '', // CSP에서 이미 허용된 경로로 변경
+  base: '/', // CSP에서 이미 허용된 경로로 변경
   plugins: [react()],
 });
