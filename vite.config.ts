39c3331920f327c3e60729c9685f6d2c71d5b4b8
SOOKIE <sookie@bluebeaker.co>
2025-01-26 18:50:38 +0900

    fix: foix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/', // CSP에서 이미 허용된 경로로 변경
   plugins: [react()],
+  resolve: {
+    alias: {
+      threex: '/ar-threex.mjs',  // 실제 경로에 맞게 수정
+    },
+  },
   build: {
     rollupOptions: {
       external: ['threex'],
