c381b05deae6627f81ff05d7093f6949a09bc1f7
SOOKIE <sookie@bluebeaker.co>
2025-02-03 08:56:07 +0900

    fix: f

--- src/components/photo/Photo.tsx
+++ src/components/photo/Photo.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 export default function Photo() {
-  return (<div className="w-full h-full min-h-[874px] relative overflow-y-scroll">
+  return (<div className="w-full h-full min-h-full relative overflow-y-scroll">
     <img className="absolute bottom-0 w-full" src={import.meta.env.VITE_PUBLIC_URL + "/background_image.png"} />
     <div className="absolute inset-0 bg-gradient-to-b from-[#344173] via-[#344173]/60 to-[#344173]/30 via-70%" />
     <Outlet />
