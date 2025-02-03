7130d636bc1b72eb51a190820c6e8b8aa8dfffd0
SOOKIE <sookie@bluebeaker.co>
2025-02-03 09:27:33 +0900

    fix: fix

--- src/components/photo/Photo.tsx
+++ src/components/photo/Photo.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 
 export default function Photo() {
-  return (<div className="w-full h-full min-h-[1074px] relative"
+  return (<div className="w-full h-full overflow-y-scroll relative"
     style={{ WebkitOverflowScrolling: "touch" }}
-
   >
-    <img className="absolute bottom-0 w-full" src={import.meta.env.VITE_PUBLIC_URL + "/background_image.png"} />
-    <div className="absolute inset-0 bg-gradient-to-b from-[#344173] via-[#344173]/60 to-[#344173]/30 via-70%" />
-    <Outlet />
+    <div className="min-h-[1074px] w-full relative">
+      <img className="absolute bottom-0 w-full" src={import.meta.env.VITE_PUBLIC_URL + "/background_image.png"} />
+      <div className="absolute inset-0 bg-gradient-to-b from-[#344173] via-[#344173]/60 to-[#344173]/30 via-70%" />
+      <Outlet /></div>
   </div>)
 }
