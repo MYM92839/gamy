5889187f4517d0e7586fad1d11ec989e926afbdf
SOOKIE <sookie@bluebeaker.co>
2025-02-04 19:46:13 +0900

    fix: fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div id="layout" className="w-dvw h-screen mx-auto relative bg-[#344173] font-pretendard overscroll-contain touch-manipulation">
+    <div id="layout" className="w-dvw min-h-screen h-screen mx-auto relative bg-[#344173] font-pretendard overscroll-contain touch-manipulation">
       <Outlet />
     </div>)
 }
