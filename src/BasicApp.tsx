047cc6af5d87bb2f84eca396e35c35ef8d0cb072
MYM92839 <134268248+MYM92839@users.noreply.github.com>
2025-02-06 17:57:46 +0000

    sadf

--- src/BasicApp.tsx
+++ src/BasicApp.tsx
@@ -327,7 +327,7 @@ function ARCanvas(props: any) {
         }}
         events={noEvents}>
         <PointerEvents />
-        <OrbitHandles zoom />
+        <OrbitHandles />
         <XR store={props.xrStoreRef.current}>
           <XROrigin position={[0, 0.5, 0]} />
           <Scene visible={props.sessionStarted} glRef={glRef} />
