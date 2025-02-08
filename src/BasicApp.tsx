fdd5d860edcb46f965daecc014272b3d4392ed33
MYM92839 <mym92839@gmail.com>
2025-02-08 14:10:23 +0900

    fix: 🐛 fix

--- src/BasicApp.tsx
+++ src/BasicApp.tsx
@@ -730,7 +730,7 @@ export default function BasicApp() {
       const cameraPos = latestCameraTransform.current.position.clone();
       const cameraQuat = latestCameraTransform.current.quaternion.clone();
       // 머리 좌표계에서의 오프셋 (전방 +Z를 사용)
-      const offset = new THREE.Vector3(0, 0, 11);
+      const offset = new THREE.Vector3(0, 0, -11);
       // 카메라의 쿼터니언의 역(인버스)을 적용하여 머리 좌표계의 오프셋을 구함
       const invQuat = cameraQuat.clone().invert();
       offset.applyQuaternion(invQuat);
