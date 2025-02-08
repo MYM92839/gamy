27b96916fa782f2b06043c3cc1bc1e66a373e5ac
MYM92839 <mym92839@gmail.com>
2025-02-08 14:34:04 +0900

    fix: 🐛 fix

--- src/BasicApp.tsx
+++ src/BasicApp.tsx
@@ -552,7 +552,7 @@ const ModalU = function ({
 
       const defaultVideoFov = 35;
       const effectiveFov = cameraFov || defaultVideoFov;
-      const addedFactor = 0.95;
+      const addedFactor = 0.9;
       const fovScale =
         (Math.tan(((effectiveFov / 2) * Math.PI) / 180) / Math.tan(((defaultVideoFov / 2) * Math.PI) / 180)) *
         addedFactor;
