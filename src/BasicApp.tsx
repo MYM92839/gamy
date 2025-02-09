b038e9ab5aeb6fe67b2ee9fc30256092fd180d9a
MYM92839 <mym92839@gmail.com>
2025-02-09 19:54:26 +0900

    fix: 🐛 asdf

--- src/BasicApp.tsx
+++ src/BasicApp.tsx
@@ -1124,8 +1124,8 @@ interface DeviceOrientationControllerProps {
 
 function DeviceOrientationController({
   isPermissionGranted,
-  // target,
-  // distance = 15,
+  target,
+  distance = 15,
   resetTrigger,
 }: DeviceOrientationControllerProps) {
   const { camera } = useThree();
@@ -1159,10 +1159,10 @@ function DeviceOrientationController({
 
   useFrame(() => {
     // 최신 카메라 방향에 따라 대상(target)에서 일정 거리(distance) 떨어진 위치 계산
-    // const targetVec = Array.isArray(target) ? new THREE.Vector3(target[0], target[1], target[2]) : target;
-    // const offset = new THREE.Vector3(0, 0, distance);
-    // offset.applyQuaternion(camera.quaternion);
-    // camera.position.copy(targetVec).add(offset);
+    const targetVec = Array.isArray(target) ? new THREE.Vector3(target[0], target[1], target[2]) : target;
+    const offset = new THREE.Vector3(0, 0, distance);
+    offset.applyQuaternion(camera.quaternion);
+    camera.position.copy(targetVec).add(offset);
     // camera.lookAt(targetVec);
   });
 
@@ -1380,9 +1380,12 @@ function IOSARCanvas(props: any) {
           setOrientationEnabled(true);
           props.logDebug('DeviceOrientation permission granted (iOS).');
         } else {
+          alert('WHY');
           props.logDebug('DeviceOrientation permission not granted.');
         }
       } catch (err) {
+        alert('WHY2');
+
         console.error('DeviceOrientation permission error:', err);
       }
     } else {
