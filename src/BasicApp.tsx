d79799cb67674356efb577c2a5569e001909a9bb
MYM92839 <mym92839@gmail.com>
2025-02-09 20:18:22 +0900

    fix: 🐛 fix

--- src/BasicApp.tsx
+++ src/BasicApp.tsx
@@ -1157,21 +1157,13 @@ function DeviceOrientationController({
     // resetTrigger를 의존성 배열에 추가하여, 값이 바뀔 때마다 이벤트 핸들러를 재설정함
   }, [camera, isPermissionGranted, resetTrigger]);
 
-  // useFrame(() => {
-  //   // 최신 카메라 방향에 따라 대상(target)에서 일정 거리(distance) 떨어진 위치 계산
-  //   const targetVec = Array.isArray(target) ? new THREE.Vector3(target[0], target[1], target[2]) : target;
-  //   const offset = new THREE.Vector3(0, 0, distance);
-  //   offset.applyQuaternion(camera.quaternion);
-  //   camera.position.copy(targetVec).add(offset);
-  //   // camera.lookAt(targetVec);
-  // });
   useFrame(() => {
     const targetVec = Array.isArray(target)
       ? new THREE.Vector3(target[0], target[1], target[2])
       : target;
     const forward = new THREE.Vector3(0, 0, -1).applyQuaternion(camera.quaternion);
     // 좌우 성분(x)만 반전시키고 스케일링 팩터를 적용합니다.
-    const factor = 0.3; // 원하는 민감도에 따라 조정하세요.
+    const factor = 0.1; // 원하는 민감도에 따라 조정하세요.
     forward.x = -forward.x * factor;
     camera.position.copy(targetVec).sub(forward.multiplyScalar(distance));
   });
