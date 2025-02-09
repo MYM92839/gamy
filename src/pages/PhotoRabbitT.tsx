afbfe188cc6f15d8b075aa84f4d28b4e93741280
MYM92839 <mym92839@gmail.com>
2025-02-09 22:59:34 +0900

    fix: 🐛 asdf

--- src/pages/PhotoRabbitT.tsx
+++ src/pages/PhotoRabbitT.tsx
@@ -39,7 +39,7 @@ export default function PhotoRabbitT() {
     <div className="w-full h-full relative">
       <PhotoPara title={`달조명을 배경으로 토끼와 함께 \n 사진을 찍을 수 있어요!`}>
         <Link
-          to={`/pl/trees?ox=${ox}&oy=${oy}&oz=${oz}&cx=${cx}&cy=${cy}&cz=${cz}&sx=${sx}&sy=${sy}&sz=${sz}&sz=${ss}`}
+          to={`/pl/moons?ox=${ox}&oy=${oy}&oz=${oz}&cx=${cx}&cy=${cy}&cz=${cz}&sx=${sx}&sy=${sy}&sz=${sz}&sz=${ss}`}
         >
           <Button title="시작" className="w-[232px] mx-auto mt-[162px]" onClick={requestDeviceOrientation} />
         </Link>
