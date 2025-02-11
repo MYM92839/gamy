fa839088378c9797cdb4a6eee2c91b2a6d9894f0
MYM92839 <mym92839@gmail.com>
2025-02-11 23:02:44 +0900

    feat: 🎸 sdf

--- src/BasicApp.tsx
+++ src/BasicApp.tsx
@@ -221,6 +221,22 @@ function Scene({
   // rabbitPosition에 맞춰 토끼를 카메라 방향 보정
   useEffect(() => {
     if (visible && groupRef.current && camera) {
+      camera.lookAt(rabbitPosition[0], rabbitPosition[1], rabbitPosition[2]);
+      camera.updateProjectionMatrix();
+
+      if (groupRef.current && glRef.current && glRef.current.camera) {
+        groupRef.current.lookAt(glRef.current.camera.position);
+
+        const offsetEuler = new THREE.Euler(0, -Math.PI / 4, 0, 'XYZ');
+        const offsetQuat = new THREE.Quaternion().setFromEuler(offsetEuler);
+        groupRef.current.quaternion.multiply(offsetQuat);
+      }
+    }
+  }, [visible, camera, rabbitPosition]);
+
+  // rabbitPosition에 맞춰 토끼를 카메라 방향 보정
+  useEffect(() => {
+    if (visible && groupRef.current && camera) {
       if (char === 'moons') {
         camera.lookAt(
           rabbitPosition[0] + cposition.x,
@@ -253,7 +269,7 @@ function Scene({
         }
       }
     }
-  }, [visible, camera, rabbitPosition, cposition]);
+  }, [cposition]);
 
   return (
     <>
