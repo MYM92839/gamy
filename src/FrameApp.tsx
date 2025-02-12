77b9942fe6c6f9d00949f016ca716583a1d3c780
MYM92839 <mym92839@gmail.com>
2025-02-12 11:03:52 +0900

    fix: 🐛 fix

--- src/FrameApp.tsx
+++ src/FrameApp.tsx
@@ -325,7 +325,7 @@ const FrameApp: React.FC<FrameAppProps> = () => {
               onLoadedMetadata={() => {
                 if (animVideoRef.current && char === 'cat') {
                   // 예: 영상 시작 시간을 3초로 설정
-                  animVideoRef.current.currentTime = 0.5;
+                  animVideoRef.current.currentTime = 0.1;
                 }
               }}
               onSeeked={() => {
