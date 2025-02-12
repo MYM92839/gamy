4e8883d67840a6ba87e6a59ec27ee39082398ba3
MYM92839 <mym92839@gmail.com>
2025-02-12 10:16:10 +0900

    fix: 🐛 fix

--- src/components/BtnImg.tsx
+++ src/components/BtnImg.tsx
@@ -5,8 +5,8 @@ interface IBtnImgProps {
 }
 export default function BtnImg({ src, title, isActive }: IBtnImgProps) {
   return (
-    <div className="relative pt-[17px] px-[24px] pb-[39px] curosor-pointer w-full h-full rounded-[20px] bg-white drop-shadow-sm text-[#344173]">
+    <div className="relative pt-[17px] px-[12px] pb-[39px] curosor-pointer w-full h-full rounded-[20px] bg-white drop-shadow-sm text-[#344173]">
       <img className={`w-full h-full object-contain ${isActive ? 'opacity-100' : 'opacity-60'}`} src={src} />
-      <div className="bottom-0 leading-[34px] text-center font-semibold text-[16px] w-full">{title}</div>
+      {title && <div className="bottom-0 leading-[34px] text-center font-semibold text-[16px] w-full">{title}</div>}
     </div>)
 }
