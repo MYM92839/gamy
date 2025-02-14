34334e7c0f1d881da4ed09908966b53ca5403a99
MYM92839 <mym92839@gmail.com>
2025-02-14 15:08:18 +0900

    fix: 🐛 fix

--- src/BasicApp6Ratio.tsx
+++ src/BasicApp6Ratio.tsx
@@ -503,7 +503,7 @@ function SceneIOS({
                 rabbitPosition[2] + cposition.z,
               ]}
               rotation={[0, -Math.PI / 4, 0]}
-              scale={cscale * 0.15}
+              scale={cscale * 0.35}
               visible={visible}
             >
               <Box
