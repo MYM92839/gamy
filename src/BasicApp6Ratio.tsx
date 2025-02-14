27356d5491f5f95d0029c1071602066c437d043f
MYM92839 <mym92839@gmail.com>
2025-02-14 15:04:55 +0900

    fix: 🐛 fix

--- src/BasicApp6Ratio.tsx
+++ src/BasicApp6Ratio.tsx
@@ -585,7 +585,7 @@ function UIOverlayIOS({
   const getPosition = () => {
     const saved = localStorage.getItem('levaValues');
     if (saved) {
-      const base = char === 'moons' ? 'https://gamy-six.vercel.app/rabbit' : 'https://gamy-six.vercel.app/tree';
+      const base = char === 'moons' ? 'https://gamy-six.vercel.app/r' : 'https://gamy-six.vercel.app/tree';
       const { oposition, sposition, cposition, sscale, cscale } = JSON.parse(saved);
       const text = `${base}?ox=${oposition.x}&oy=${oposition.y}&oz=${oposition.z}&cx=${cposition.x}&cy=${cposition.y}&cz=${cposition.z}&sx=${sposition.x}&sy=${sposition.y}&sz=${sposition.z}&ss=${sscale}&cs=${cscale}`;
       navigator.clipboard
@@ -625,7 +625,7 @@ function UIOverlayIOS({
         }}
         onClick={() => {
           window.location.href =
-            char === 'moons' ? 'https://gamy-six.vercel.app/rabbit' : 'https://gamy-six.vercel.app/tree';
+            char === 'moons' ? 'https://gamy-six.vercel.app/r' : 'https://gamy-six.vercel.app/tree';
         }}
       >
         <Back />
