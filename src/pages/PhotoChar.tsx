10abf53ce2252c568d8587fe29634d4280ab5a9e
MYM92839 <mym92839@gmail.com>
2025-02-26 19:26:37 +0900

    fix: 🐛 fix

--- src/pages/PhotoChar.tsx
+++ src/pages/PhotoChar.tsx
@@ -40,7 +40,7 @@ export default function PhotoChar() {
     <div className="w-full h-full relative">
       <PhotoPara title={`원주의 캐릭터와 함께 \n 사진을 찍을 수 있어요!`}>
         <Link to={'/frame/kokoang'}>
-          <Button title="코코앙과 찍기" className="w-[232px] mx-auto mt-[124px]" />
+          <Button title="코코앙과 찍기" className="w-[232px] mx-auto mt-[94px]" />
         </Link>
         <Link to={'/frame/cat'}>
           <Button title="고양이와 찍기" className="w-[232px] mx-auto mt-[22px]" />
